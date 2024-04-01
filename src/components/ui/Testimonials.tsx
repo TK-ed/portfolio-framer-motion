2c1f6e196bb4075417f0e89bf1352e07a72b824c
TK-ed <tharunstark11@gmail.com>
2024-04-01 21:17:03 +0530

    Fixed Deployment errors while linting

--- src/components/ui/Testimonials.tsx
+++ src/components/ui/Testimonials.tsx
@@ -3,6 +3,9 @@ import { InfiniteMovingCards } from "./infinite-moving-cards";
 import { motion } from "framer-motion";
 
 export default function Testimonials({ testimonials }: any) {
+  const testimonialsText =
+    'Testimonials are the best way to prove that your products work. They help potential buyers feel more <span className="underline-offset-2 text-green-400">confident</span> in their purchase decision. - Neil Patel';
+
   return (
     <div id="testimonials" className="my-16">
       <motion.h1
@@ -12,9 +15,10 @@ export default function Testimonials({ testimonials }: any) {
         transition={{ delay: 0.8, duration: 1, ease: "easeInOut" }}
         className="text-7xl "
       >
-        "Testimonials are the best way to prove that your products work. They
-        help potential buyers feel more <span className="underline-offset-2 text-green-400">confident</span> in their purchase decision." -
-        Neil Patel
+        &quot;Testimonials are the best way to prove that your products work.
+        They help potential buyers feel more{" "}
+        <span className="underline-offset-auto decoration-green-400 text-green-400">confident</span> in
+        their purchase decision.&quot; - Neil Patel
       </motion.h1>
       <motion.div
         className="my-[6rem]"
